76afeb79dce15bc8cf7c948a28348d23eef27730
Oscar Roa <oshardy3@gmail.com>
2015-08-17 22:56:12 -0500

    Url y vista para la página de login agregada.

--- appCheluber/views.py
+++ appCheluber/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(req):
+    return render(req, 'login.html')
+
 def landingPage(req):
     return render(req, 'index.html')
