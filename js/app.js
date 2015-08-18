09b35649bbb46eef1b3c11321148e90bb118a1a0
Omar Regino <omarjrz21@gmail.com>
2015-08-17 20:08:02 -0500

    Se agrega marcador de Google Maps

--- js/app.js
+++ js/app.js
@@ -6,9 +6,16 @@ $(document).ready(function(){
 	    		center: LatLng,
 	    		image: 'img/marker.png',
 				scrollwheel: false,	
-				styles: [{"featureType":"all","elementType":"all","stylers":[{"hue":"#ffbb00"}]},{"featureType":"all","elementType":"geometry.fill","stylers":[{"hue":"#ffbb00"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"hue":"#ffbb00"}]}],
+				styles: [{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#00FF6A"},{"saturation":-1.0989010989011234},{"lightness":11.200000000000017},{"gamma":1}]}],
 				zoom: 15,
 	    	};
+	    	
+	    	 var contentString = '<div class="card-panel teal">' +
+	    	 					 	 '<span class="white-text">' +
+	    	 							 'Aqui...' +
+	    	 						 '</span>' +
+	    	 					 '</div>';
+
 	    	var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
 	    	var marker = new google.maps.Marker({
@@ -16,7 +23,7 @@ $(document).ready(function(){
 	    		map: map,
 	    		icon: mapOptions.image,
 	    	});
-
+	    	
 	    });
 	}
 });
