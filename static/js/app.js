f064c6f90bb24298f6d2f304a67a51be67a35c2b
Oscar Roa <oshardy3@gmail.com>
2015-08-18 20:21:59 -0500

    Imagen de marcador de ubicación actual corregida.

--- static/js/app.js
+++ static/js/app.js
@@ -4,12 +4,12 @@ $(document).ready(function(){
 			var LatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 	    	var mapOptions = {
 	    		center: LatLng,
-	    		image: 'img/marker.png',
-				scrollwheel: false,	
+	    		image: 'http://i.stack.imgur.com/cdiAE.png',
+				scrollwheel: false,
 				styles: [{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#00FF6A"},{"saturation":-1.0989010989011234},{"lightness":11.200000000000017},{"gamma":1}]}],
 				zoom: 15,
 	    	};
-	    	
+
 	    	 var contentString = '<div class="card-panel teal">' +
 	    	 					 	 '<span class="white-text">' +
 	    	 							 'Aqui...' +
@@ -23,7 +23,7 @@ $(document).ready(function(){
 	    		map: map,
 	    		icon: mapOptions.image,
 	    	});
-	    	
+
 	    });
 	}
 });
